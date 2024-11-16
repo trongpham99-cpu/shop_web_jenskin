437d11d87d08ff7dd7bfe2d10888828e032b599e
trong.phamtranduc@gmail.com <trong.phamtranduc@gmail.com>
2024-11-16 15:03:47 +0700

    init source code

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "Hello World",
 		})
 	})
 	r.GET("/api/v1/products", func(c *gin.Context) {
